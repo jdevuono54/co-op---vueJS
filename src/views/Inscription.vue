8316fff277e50bb138a898cd3096c65c785cfa90
Jacques <jacques.devuono@gmail.com>
2019-12-15 02:57:20 +0100

    création de la vue de login

---
+++ src/views/Inscription.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <div>
+            <input type="text" name="mail" placeholder="Adresse mail">
+        </div>
+        <div>
+            <input type="text" name="password" placeholder="Mot de passe">
+        </div>
+        <div>
+            <button type="button">Connexion</button>
+            <router-link to="/Inscription">S'inscrire</router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Inscription"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,26 @@
+<template>
+    <form>
+        <div>
+            <div>
+                <input type="text" name="mail" placeholder="Adresse mail">
+            </div>
+            <div>
+                <input type="text" name="password" placeholder="Mot de passe">
+            </div>
+            <div>
+                <button type="button">Connexion</button>
+                <router-link to="/Inscription">S'inscrire</router-link>
+            </div>
+        </div>
+    </form>
+</template>
+
+<script>
+    export default {
+        name: "Login"
+    }
+</script>
+
+<style scoped>
+
+</style>
