d7c5cd594c56eb21aca60a2045fd804f92afc71b
Jacques <jacques.devuono@gmail.com>
2019-12-26 00:00:03 +0100

    css tableau ok

--- src/components/channel/Conversations.vue
+++ src/components/channel/Conversations.vue
@@ -1,11 +1,18 @@
 <template>
     <div class="Conversation">
+        <b-row>
         <table class="table table-hover table-striped">
             <thead class="thead-light">
             <tr>
                 <th scope="col">
-                    Conversations
-                    <b-button class="btn btn-info" v-b-modal.modal-conversation>Nouvelle conversation</b-button>
+                    <b-row>
+                    <b-col sm="12" md="6" class="table_title">
+                        Conversations
+                    </b-col>
+                    <b-col sm="12" md="6" id="btn_new_conv">
+                        <b-button class="btn btn-info" v-b-modal.modal-conversation>Nouvelle conversation</b-button>
+                    </b-col>
+                    </b-row>
                 </th>
             </tr>
             </thead>
@@ -18,6 +25,7 @@
                 </tbody>
             </div>
         </table>
+        </b-row>
     </div>
 
 </template>
@@ -42,11 +50,29 @@
 </script>
 
 <style scoped>
+    .table_title{
+        margin-bottom: auto;
+        margin-top: auto;
+        font-size: 1.2em;
+    }
     .table-responsive {
         max-height: 600px;
     }
-
     td {
         width: 100vw;
+    }
+
+
+    th> div:nth-child(1){
+        text-align: left;
+    }
+    #btn_new_conv{
+        text-align: right;
+    }
+
+    @media (max-width: 768px) {
+        th> div:nth-child(1),#btn_new_conv{
+            text-align: center;
+        }
     }
 </style>
