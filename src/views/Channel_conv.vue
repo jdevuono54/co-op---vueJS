266e2e928ecf2296dd1cabb0a828231fcf049c15
Jacques <jacques.devuono@gmail.com>
2019-12-26 02:02:04 +0100

    envoie message ok

--- src/views/Channel_conv.vue
+++ src/views/Channel_conv.vue
@@ -1,10 +1,40 @@
 <template>
-    <p>test</p>
+    <div>
+        <input type="text" v-model="message">
+        <button type="button" @click="ajouterMessage">aaaa</button>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "Channel_conv"
+        name: "Channel_conv",
+        data: function() {
+            return {
+                message:null,
+                error:null,
+            }
+        },
+        methods:{
+            makeToast() {
+                this.toastCount++
+                this.$bvToast.toast(`This is toast number ${this.toastCount}`, {
+                    title: 'BootstrapVue Toast',
+                    autoHideDelay: 50000
+                })
+            },
+            ajouterMessage(){
+                this.$http.post('channels/'+this.$route.params.id+'/posts?token='+this.$store.state.user.token, {
+                    message:this.message,
+                }).then((response) => {
+                    this.$parent.conversations.push(response.data)
+                    this.$bvModal.hide('modal-conversation')
+                }).catch((e) => {
+                    this.error = e.response.data.message
+                    this.makeToast()
+                })
+
+            }
+        }
     }
 </script>
 
